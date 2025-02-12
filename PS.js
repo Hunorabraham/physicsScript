c61b1d8a6f2d589decc4b2cc7c6a2700d6093866
HunorAbraham <hunor335@gmail.com>
2025-02-12 16:09:30 +0100

    basic Vec2, Mat2 functions

--- PS.js
+++ PS.js
@@ -10,11 +10,20 @@ class Mat2{
         this.yx = yx;
         this.yy = yy;
     }
+    static rotation(angle){
+        return new Mat2(Math.cos(angle), - Math.sin(angle), Math.sin(angle), Math.cos(angle));
+    }
     transpose(){
         return new Mat2(this.xx, this.yx, this.xy, this.yy);
     }
     inverse(){
         console.error("I forbor");
+    }
+    transposeInto(){
+        let temp = this.xy;
+        this.xy = this.yx;
+        this.yx = temp;
+        return this;
     }
 }
 class Vec2{
@@ -26,6 +35,25 @@ class Vec2{
     sub(v){return new Vec2(this.x - v.x, this.y - v.y);}
     scale(c){return new Vec2(this.x*c, this.y*c);}
     mul(M){return new Vec2(this.x*M.xx + this.y*M.xy, this.y*M.yy + this.x*M.yx);}
-
+    addInto(v){
+        this.x += v.x;
+        this.y += v.y;
+        return this;
+    }
+    subInto(v){
+        this.x -= v.x;
+        this.y -= v.y;
+        return this;
+    }
+    scaleInto(c){
+        this.x *= c;
+        this.y *= c;
+        return this;
+    }
+    mulInto(M){
+        this.x = this.x*M.xx + this.y*M.xy;
+        this.y = this.y*M.yy + this.x*M.yx;
+        return this;
+    }
 }
 

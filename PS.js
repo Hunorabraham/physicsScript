88fc89b70da7083b8cb9691426b27a113c2acf52
HunorAbraham <hunor335@gmail.com>
2025-02-12 16:02:34 +0100

    vecor and matrix classes

--- PS.js
+++ PS.js
@@ -3,3 +3,29 @@ class physics{
         console.log("This is PS.js ver 0.0.1");
     }
 }
+class Mat2{
+    constructor(xx, xy, yx ,yy){
+        this.xx = xx;
+        this.xy = xy;
+        this.yx = yx;
+        this.yy = yy;
+    }
+    transpose(){
+        return new Mat2(this.xx, this.yx, this.xy, this.yy);
+    }
+    inverse(){
+        console.error("I forbor");
+    }
+}
+class Vec2{
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+    }
+    add(v){return new Vec2(this.x + v.x, this.y + v.y);}
+    sub(v){return new Vec2(this.x - v.x, this.y - v.y);}
+    scale(c){return new Vec2(this.x*c, this.y*c);}
+    mul(M){return new Vec2(this.x*M.xx + this.y*M.xy, this.y*M.yy + this.x*M.yx);}
+
+}
+
